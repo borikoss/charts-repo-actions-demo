cc7e4e1e52755fb4c2291c2489a9c2c0a77dee09
Boris Kossev <ood5fe@bosch.com>
2023-10-09 12:06:12 +0200

    fix pr commit

--- .github/workflows/utils/create-pr.sh
+++ .github/workflows/utils/create-pr.sh
@@ -80,7 +80,7 @@ if [[ `git status --porcelain | head -1` ]]; then
     owner_repo="${DEST_REPO#https://github.com/}"
     echo $owner_repo
     export GITHUB_TOKEN=$TOKEN
-    pr_response=$(gh pr create --repo $repo_url --base $DEST_BRANCH --head $deploy_branch_name --title "deployment $DEPLOY_ID" --body "Deploy to $DEST_BRANCH")
+    pr_response=$(gh pr create --repo $repo_url --base $DEST_BRANCH --head $deploy_branch_name --title "Deploy ID $DEPLOY_ID to DEST_FOLDER $DEST_FOLDER on DEST_BRANCH $DEST_BRANCH" --body "Deploy to DEST_FOLDER $DEST_FOLDER")
     echo $pr_response
     if [[ "$AUTO_MERGE" == "Y" ]]; then
         pr_num="${pr_response##*pull/}"
